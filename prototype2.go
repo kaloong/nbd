d33137ab62b10b609b5367f7427bbb9a4be78030
kaloong <16438208+kaloong@users.noreply.github.com>
2019-06-30 12:18:15 +0100

    Completed prototype2 which return previous and next biz day. Next up is Bank Holiday and test script.

--- prototype2.go
+++ prototype2.go
@@ -7,44 +7,56 @@ import (
     "flag"
 )
 
-//Function to return last Business day
-// func dummy() time.Weekday {
-func getLastBusinessDay( t time.Time ) time.Weekday {
-    fmt.Println("- Its a :", time.Time(t).Weekday())
+//Wrapper function of getBizDay
+func getLastBizDay(t time.Time) time.Time  {
+    offset_list := []int { -2,-3,-1,-1,-1,-1,-1,0 }
+    return getBizDay( offset_list, t )
+}
+
+//Wrapper function of getBizDay
+func getNextBizDay(t time.Time) time.Time  {
+    offset_list := []int { 1,1,1,1,1,3,2,0 }
+    return getBizDay( offset_list, t )
+}
+
+//Function to Business day base on offset
+func getBizDay( offset_list []int , t time.Time ) time.Time {
+    var offset = 0
     switch enquiry_date := time.Time(t).Weekday() ; {
     //switch enquiry_date := time.Now().Weekday() ; {
     // if it is Sunday
     case enquiry_date == 0 :
         fmt.Println("- case 0",enquiry_date+5)
-        return enquiry_date+5
+        offset = offset_list[0] 
     // if it is Monday
     case enquiry_date == 1 :
         fmt.Println("- case 1",enquiry_date+4)
-        return enquiry_date+4
+        offset = offset_list[1] 
     // if it is Tuesday
     case enquiry_date == 2 :
         fmt.Println("- case 2",enquiry_date-1)
-        return enquiry_date-1
+        offset = offset_list[2] 
     // if it is Wednesday
     case enquiry_date == 3 :
         fmt.Println("- case 3",enquiry_date-1)
-        return enquiry_date-1
+        offset = offset_list[3] 
     // if it is Thursday
     case enquiry_date == 4 :
         fmt.Println("- case 4",enquiry_date-1)
-        return enquiry_date-1
+        offset = offset_list[4] 
     // if it is Friday
     case enquiry_date == 5 :
         fmt.Println("- case 5",enquiry_date-1)
-        return enquiry_date-1
+        offset = offset_list[5] 
     // if it is Saturday
     case enquiry_date == 6 :
         fmt.Println("- case 6",enquiry_date-1)
-        return enquiry_date-1
+        offset = offset_list[6] 
     default:
         fmt.Println("- case d",enquiry_date+1)
-        return enquiry_date-1
+        offset = offset_list[7] 
     }
+    return t.AddDate(0,0,offset)
 }
 
 func main() {
@@ -54,7 +66,7 @@ func main() {
     }
 
     getCmd := flag.NewFlagSet("get", flag.ExitOnError)
-    tzFlag := getCmd.String("t","","Timezone name")
+    tzFlag := getCmd.String("t","Greenwich","Timezone name")
     bdFlag := getCmd.String("b","","Business Date name")
     hpFlag := getCmd.String("h","","Get help")
 
@@ -90,7 +102,10 @@ func main() {
     for i := range formats {
         if timeStamp, err := time.Parse(formats[i], *bdFlag); err == nil {
             //t := time.Now().Add(*bdFlag)
-            fmt.Printf("- It was a: %v \n", getLastBusinessDay(timeStamp.In(loc)) )
+            prev_bizday := getLastBizDay(timeStamp.In(loc))
+            next_bizday := getNextBizDay(timeStamp.In(loc))
+            fmt.Printf("- Previous business day : %v\t%v\n", prev_bizday.Weekday(), prev_bizday.Format("2006-01-02 MST"))
+            fmt.Printf("-     Next business day : %v\t%v\n", next_bizday.Weekday(), next_bizday.Format("2006-01-02 MST"))
         }
     }
 }
